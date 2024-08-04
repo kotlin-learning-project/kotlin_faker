4e1feec2b8c97eae93fb9a2a2636db97070a3c40
livingstoantony <livingstonantony70@gmail.com>
2024-08-04 10:20:37 +0530

    FakeDataProvider.kt updated

--- src/main/kotlin/faker/FakeDataProvider.kt
+++ src/main/kotlin/faker/FakeDataProvider.kt
@@ -5,11 +5,11 @@ import io.github.serpro69.kfaker.Faker
 fun getEmployeeFakeData(faker: Faker):String ="{\n" +
         "  \"status\": \"success\",\n" +
         "  \"data\": {\n" +
-        "    \"id\": 1,\n" +
+        "    \"id\": ${(1..100).random()},\n" +
         "    \"employee_first_name\": ${faker.name.firstNameWithNullAndEmpty()},\n" +
         "    \"employee_last_name\": ${faker.name.lastNameWithNullAndEmpty()},\n" +
-        "    \"employee_salary\": 320800,\n" +
-        "    \"employee_age\": 61,\n" +
+        "    \"employee_salary\": ${(300000..399999).random()},\n" +
+        "    \"employee_age\": ${(20..61).random()},\n" +
         "    \"profile_image\": \"\"\n" +
         "  },\n" +
         "  \"message\": \"Successfully! Record has been fetched.\"\n" +
